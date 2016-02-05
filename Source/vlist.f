157130e2d2a7097b02f4822703f465e9831e6ffe
JaroCamphuijsen <jjecamphuijsen@gmail.com>
2016-02-05 15:05:36 +0100

    Fix vlist for three dimensions.

--- Source/vlist.f
+++ Source/vlist.f
@@ -9,21 +9,38 @@
 
       INTEGER nlist(npmax), list(npmax, npmax)
       INTEGER i, j
-      DOUBLE PRECISION xr
+      DOUBLE PRECISION xr, yr, zr, r
 
       DO i = 1, NPART
          nlist(i) = 0
          XV(i) = X(i)
+         YV(i) = Y(i)
+         ZV(i) = Z(i)
       END DO
       DO i = 1, NPART - 1
          DO j = i + 1, NPART
             xr = X(i) - X(j)
+            yr = Y(i) - Y(j)
+            zr = Z(i) - Z(j)
+
             IF (xr.GT.hbox) THEN
                xr = xr - box
             ELSE IF (xr.LT.-hbox) THEN
                xr = xr + box
             END IF
-            IF (abs(xr).LT.rv) THEN
+            IF (yr.GT.hbox) THEN
+               yr = yr - box
+            ELSE IF (yr.LT.-hbox) THEN
+               yr = yr + box
+            END IF
+            IF (zr.GT.hbox) THEN
+               zr = zr - box
+            ELSE IF (zr.LT.-hbox) THEN
+               zr = zr + box
+            END IF
+
+            r = SQRT(xr**2 + yr**2 + zr**2)
+            IF (abs(r).LT.rv) THEN
                nlist(i) = nlist(i) + 1
                nlist(j) = nlist(j) + 1
                list(i, nlist(i)) = j
